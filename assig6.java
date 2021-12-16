5f65db7e49e800b2eacad292f9c0ed857b1ff308
loai nahari (441012440) <91420944+loai7@users.noreply.github.com>
2021-12-16 09:16:28 -0800

    Create assig6.java

---
+++ assig6.java
@@ -0,0 +1,25 @@
+  public class Assignment {
+  public static int addInt(int x, int y) {
+    return x + y;
+  }
+    static int supInt(int x, int y) {
+    return x - y;
+  }
+    static int multiInt(int x, int y) {
+    return x * y;
+  }
+    static int divInt(int x, int y) {
+    return x / y;
+  }
+
+
+
+
+public static void main(String[] args) {
+    System.out.println("+: " + addInt(1, 5));
+    System.out.println( supInt(1, 5));
+     System.out.println(multiInt(1, 5));
+    System.out.println(divInt(1, 5));
+	}
+}
+
