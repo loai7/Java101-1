d84c7319e72df63fe277009e9775a1fa314b03ac
loai nahari (441012440) <91420944+loai7@users.noreply.github.com>
2021-12-16 09:19:04 -0800

    Create assig8.java

---
+++ assig8.java
@@ -0,0 +1,14 @@
+public class Assignment { 
+     public static void main (String[] args) {
+        
+        String name = "Loai Alnahari";
+        for (int counter = 0; counter <= name.length() - 1; ++counter){ 
+            if(name.charAt(counter) == 'a' || name.charAt(counter) == 'A'){
+                System.out.print('x');
+                
+                }else { 
+                    System.out.print (name.charAt(counter));
+}
+}
+}
+}
