bc0c968ae5afefd0e16736a04a77a70ec2d16540
loai nahari (441012440) <91420944+loai7@users.noreply.github.com>
2021-12-16 09:14:44 -0800

    Create assig5.java

---
+++ assig5.java
@@ -0,0 +1,10 @@
+public class Assignment {
+    public static void main (String[] args) {
+    String name = "HaniAlthubaiti";
+    
+     int mid = name.length()/2 ; 
+    
+    System.out.println(name.charAt(mid));
+    
+    }
+}
