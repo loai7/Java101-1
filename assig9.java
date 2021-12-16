530e3d5b60f5b30ad01013cd5c42ab72ba65ba39
loai nahari (441012440) <91420944+loai7@users.noreply.github.com>
2021-12-16 09:20:45 -0800

    Create assig9.java

---
+++ assig9.java
@@ -0,0 +1,11 @@
+public class Assignment {
+    public static void main (String[] args) {
+   String[] cars = {"Toyota","Mazda","x","y","z"};
+   for (int index = 0;index <cars.length; ++index){
+       System.out.println(cars[index]);
+   }
+   for(String x : cars){
+       System.out.println(x);
+   }
+   }
+   }
