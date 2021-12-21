7afd8d5af1d6921d4de5610376de1df1b4736a9d
loai nahari (441012440) <91420944+loai7@users.noreply.github.com>
2021-12-20 22:16:12 -0800

    Create exam.java

---
+++ exam.java
@@ -0,0 +1,20 @@
+public class Main {  
+    public static void main(String[] args) {
+        
+        
+        
+        int counter = 100;
+        while(counter>=0){
+            
+        
+            
+            
+        if (counter %2==0) { 
+            System.out.println(counter+"%2 is even");
+        }
+        else  {
+            System.out.println(counter+"%2 is odd");
+         } --counter; 
+        }
+    }
+}
